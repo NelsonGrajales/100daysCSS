2b6520200c0efe07f4c776a12ac3986f59759b98
NelsonGrajales <ngrajales2332@gmail.com>
2024-03-05 23:42:34 -0500

    opcion desplegable

--- Day13/day13.js
+++ Day13/day13.js
@@ -1,10 +1,11 @@
 const info = document.querySelector('.info');
 const fondo = document.querySelector('.fondo')
 const buttonElements = document.querySelectorAll('.mas');
-
+const close = document.querySelector('.close')
 buttonElements.forEach((button) => {
     button.addEventListener('click', () => {
         info.classList.add('active');
         fondo.classList.add('active');
+        close.classList.add('active');
     });
 });

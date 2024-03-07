4e3e98108e6a460ba5a0a83d07f7c523d8567e9a
NelsonGrajales <ngrajales2332@gmail.com>
2024-03-06 23:46:40 -0500

    info lista

--- Day13/day13.js
+++ Day13/day13.js
@@ -2,19 +2,22 @@ const info = document.querySelector('.info');
 const fondo = document.querySelector('.fondo')
 const buttonElements = document.querySelectorAll('.mas');
 const close = document.querySelector('.close')
+const perfil = document.querySelector('.perfil');
 
 
 buttonElements.forEach((button) => {
     button.addEventListener('click', () => {
-        info.classList.add('active');
-        fondo.classList.add('active');
-        close.classList.add('active');
+        info.style.animation="subir 0.7s ease-in-out forwards"
+        fondo.style.animation="bajar 0.7s ease-in-out forwards"
+        close.style.animation="bajar 1.3s ease-in-out forwards"
+        perfil.style.animation="img 1.2s ease-in-out forwards"
     });
 });
 
 
-close.addEventListener('click', () => {
-    fondo.classList.remove('active');
-    close.classList.remove('active');
-    info.classList.remove('active')
+close.addEventListener('click', () => { 
+    info.style.animation="subir2 0.7s ease-in-out forwards"
+    fondo.style.animation="bajar2 1s ease-in-out forwards"
+    close.style.animation="bajar2 0.6s ease-in-out forwards"
+    perfil.style.animation="img2 0.6s ease-in-out forwards"
 })
